fba385553efcd4fb40d00773a5bf42b53a5e75b5
ko-ryo <ryotak@RyotanoMacBook-Pro.local>
2017-04-18 18:42:10 +0900

    edit files

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,6 @@
 class GroupsController < ApplicationController
 
-  before_action :set_group, only: [:edit, :update,]
-
-  def index
-  end
+  before_action :set_group, only: [:edit, :update]
 
   def show
     @ids = current_user.group_ids
