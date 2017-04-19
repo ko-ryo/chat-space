5d2fd7d31ee536375b09632d2bbdba0f0997bd60
ko-ryo <ryotak@RyotanoMacBook-Pro.local>
2017-04-19 20:40:25 +0900

    add a function to send messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,29 @@
 class MessagesController < ApplicationController
+
+  before_action :set_group, only: [:index, :create]
+
   def index
-    @group = Group.find(params[:group_id])
     @groups = current_user.groups
+    @message = Message.new
+    @messages = Message.all
+  end
+
+  def create
+    @message = Message.new(body: message_params[:body], group_id: @group.id, user_id: current_user.id)
+    if @message.save
+      redirect_to :back, success: "メッセージの送信に成功しました。"
+    else
+      flash.now[:warning] = "メッセージの送信に失敗しました。"
+      render :new
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :groups_users
   has_many :groups, through: :groups_users
+  has_many :messages
   accepts_nested_attributes_for :groups_users
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
