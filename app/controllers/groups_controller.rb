1ebb624ceaf4a1bcdcd4941b4f3d4ec26f072274
ko-ryo <ryotak@RyotanoMacBook-Pro.local>
2017-04-21 21:35:00 +0900

    edit controllers and views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,7 @@ class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
 
   def show
-    @groups = current_user.groups
+    @groups = current_user.groups.includes(:messages)
   end
 
   def new
