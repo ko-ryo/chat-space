59553a6112f67626151b2f9df1da087f1c9fb81d
ko-ryo <ryotak@RyotanoMacBook-Pro.local>
2017-04-22 14:01:50 +0900

    edit group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,5 +3,4 @@ class Group < ApplicationRecord
   has_many :users, through: :groups_users
   has_many :messages
   validates :name, presence: :true
-  accepts_nested_attributes_for :groups_users
 end
